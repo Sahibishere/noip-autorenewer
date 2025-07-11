ba1cff5e1f5508fb9dae06e23dbf45f9909651e6
Sahibishere <131002137+Sahibishere@users.noreply.github.com>
2025-07-11 16:18:40 -0700

    Update renew.py

--- renew.py
+++ renew.py
@@ -5,11 +5,11 @@ from sys import argv
 from time import sleep
 
 import pyotp
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
