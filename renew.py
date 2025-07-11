20160eb4dd5853730964adfbfd8231eddb397f07
Sahibishere <131002137+Sahibishere@users.noreply.github.com>
2025-07-11 13:45:23 -0700

    Update renew.py

--- renew.py
+++ renew.py
@@ -7,10 +7,9 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
-
 browser_options = ChromeOptions()
 browser_options.add_argument("--headless")
 browser_options.add_argument("--no-sandbox")
